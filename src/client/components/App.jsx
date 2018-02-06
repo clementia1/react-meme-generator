75b828c7e1210033b848d1dcdaec9f13ddaf8182
clementia1 <grininma0@gmail.com>
2018-02-06 17:04:15 +0200

    +material design lite

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import Canvas from './canvas';
+import 'material-design-lite/material.css';
+import 'material-design-lite/material.min.js';
+
 
 class App extends React.Component {
    constructor(props) {
@@ -39,13 +42,36 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>        
-        <input type="file" onChange={this.handleUploadImage} />
-        Заголовок: <input type="text" onChange={this.handleTitleChange} />
-        Ширина: <input type="range" min="0" max="1000" step="10" value={this.state.width} onChange={this.handleWidthChange} />
-        <div className="displayImage"><img></img></div> 
-        <Canvas image={this.state.image} title={this.state.title} width={this.state.width} />        
-      </div>
+        <div>
+            <div className="mdl-grid">
+                <div className="mdl-cell mdl-cell--12-col">
+                    <input type="file" onChange={this.handleUploadImage} /> 
+                      <div className="mdl-textfield mdl-js-textfield">
+                        <input className="mdl-textfield__input" type="text" id="title" onChange={this.handleTitleChange} />
+                        <label className="mdl-textfield__label" htmlFor="title">Type your title</label>
+                      </div>
+                    <p className="rangeCanvasWidth" id="rangeCanvasWidth"> <input className="mdl-slider mdl-js-slider" type="range" min="0" max="1000" step="10" value={this.state.width} onChange={this.handleWidthChange} /> </p>
+                    <div className="mdl-tooltip" htmlFor="rangeCanvasWidth">
+                    Adjust canvas width
+                    </div>
+                </div>
+            </div>
+            <div className="mdl-grid">
+                <div className="mdl-cell mdl-cell--3-col">
+                    <div className="mdl-grid">
+                        <div className="mdl-cell mdl-cell--3-col mdl-shadow--2dp imgBox"><img src="https://pbs.twimg.com/profile_images/848395594590814208/_TtPuzHs.jpg" alt="альтернативный текст" /></div>
+                        <div className="mdl-cell mdl-cell--3-col mdl-shadow--2dp imgBox"><img src="https://pbs.twimg.com/profile_images/848395594590814208/_TtPuzHs.jpg" alt="альтернативный текст" /></div>
+                        <div className="mdl-cell mdl-cell--3-col mdl-shadow--2dp imgBox"><img src="https://pbs.twimg.com/profile_images/848395594590814208/_TtPuzHs.jpg" alt="альтернативный текст" /></div>
+                        <div className="mdl-cell mdl-cell--3-col mdl-shadow--2dp imgBox"><img src="https://pbs.twimg.com/profile_images/848395594590814208/_TtPuzHs.jpg" alt="альтернативный текст" /></div>
+                        <div className="mdl-cell mdl-cell--3-col mdl-shadow--2dp imgBox"><img src="upload/Pepe_pls.png" alt="альтернативный текст" /></div>
+                        <div className="mdl-cell mdl-cell--3-col mdl-shadow--2dp imgBox"><img src="upload/rageface.jpg" alt="альтернативный текст" /></div>
+                    </div>
+                </div>
+                <div className="mdl-cell mdl-cell--9-col">
+                    <Canvas image={this.state.image} title={this.state.title} width={this.state.width}/>
+                </div>
+            </div>
+        </div>
     );
   }
 }
